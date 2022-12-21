fa490a2fdee6239f92c74b43c244caef553d2dd1
k0ctrh <koctrh@gmail.com>
2022-12-22 01:02:27 +0300

    Homework_cloud_testapp

---
+++ install_mongod.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo apt-get update
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sleep 30
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl enable mongod
+sudo systemctl status mongod
--- startup_script.sh
+++ startup_script.sh
@@ -1,11 +1,3 @@
-# k0ctrh_infra
-k0ctrh Infra repository
-
-bastion_IP = 158.160.53.65
-someinternalhost_IP = 10.128.0.19
-
-ssh -J user@bastion_ip user@someinternalhost_ip
-
 #!/bin/bash
 yc compute instance create \
   --name reddit-app \
