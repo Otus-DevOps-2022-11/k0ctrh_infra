dcb214508e232886b7475950d0ac579a48614e4e
k0ctrh <96119971+k0ctrh@users.noreply.github.com>
2022-12-22 15:25:14 +0300

    Update install_mongodb.sh
    test

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sleep 60
