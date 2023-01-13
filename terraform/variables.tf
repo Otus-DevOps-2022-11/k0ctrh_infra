509e404ca0b56ea7653b0dad2f6cd50b58a60295
koctrh <koctrh@gmail.com>
2023-01-13 12:22:59 +0300

    HW-terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable cloud_id{
+  description = "cloud"
+}
+variable folder_id {
+  description = "folder"
+}
+variable zone {
+  description = "zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  description = "Private key file"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id{
+  description = "subnet"
+}
+variable service_account_key_file{
+  description = "key .json"
+}
+
